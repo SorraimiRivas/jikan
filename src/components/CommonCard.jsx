c80bf71b4bb0b3f8c46748c0a976c95aea5863cc
Sorraimi Rivas <smorel@applaudostudios.dev>
2023-02-07 14:16:24 -0400

    fixed nativewind bug where it would not style components

--- src/components/CommonCard.jsx
+++ src/components/CommonCard.jsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 const CommonCard = ({ title }) => {
   return (
-    <View className="bg-black">
-      <Text className="font-bold bg-gray-500">{title}</Text>
+    <View className="h-56 w-32 rounded m-5">
+      <Image className="w-full h-4/5 bg-white rounded" />
+      <Text className="font-bold bg-zinc-500  rounded">{title}</Text>
     </View>
   );
 };
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,28 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React, { Component } from "react";
 
 export class Test extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Test</Text>
+      <View>
+        <Text className="">Test</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: 50,
-    width: "100%",
-  },
-
-  text: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-    backgroundColor: "gray",
-  },
-});
 
 export default Test;
